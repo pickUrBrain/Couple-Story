0e26c01206dcd766e3e4131c06507f404b9b951a
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-11-06 15:49:56 -0500

    drawPolygon && drawCircle && instance variables for shape object

--- src/Shape.java
+++ src/Shape.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.core.PConstants;
 import processing.core.PVector;
 
 /**
@@ -7,22 +8,57 @@ import processing.core.PVector;
  *         {@link https://processing.org/examples/regularpolygon.html}
  *         {@link https://processing.org/examples/morph.html}
  */
-public class Shape extends PApplet {
-	// store the vertices for different shapes
-	ArrayList<PVector> hexagon = new ArrayList<PVector>();
-	ArrayList<PVector> pentagon = new ArrayList<PVector>();
-	ArrayList<PVector> square = new ArrayList<PVector>();
-	ArrayList<PVector> triangle = new ArrayList<PVector>();
-	ArrayList<PVector> circle = new ArrayList<PVector>();
+public class Shape {
 
-	void polygon(float x, float y, float radius, int npoints) {
-		float angle = TWO_PI / npoints;
-		beginShape();
-		for (float a = 0; a < TWO_PI; a += angle) {
-			float sx = x + cos(a) * radius;
-			float sy = y + sin(a) * radius;
-			vertex(sx, sy);
-		}
-		endShape(CLOSE);
+	// This boolean variable will control if we are morphing to a circle or square
+	boolean isSquare = false;
+
+	Body body;
+	PApplet app;
+	int color;
+	float centerX;
+	float centerY;
+	float rad; // radius for the shape
+	float angle = 0;
+	float aVelocity = 0;
+	float aAcceleration = 0;
+	ArrayList<PVector> vertices = new ArrayList<PVector>();
+
+	public Shape(PApplet app) {
+		this.app = app;
+
+		app.colorMode(PApplet.HSB);
+		color = app.color(app.random(255), 255, 255);
 	}
+
+	public void update(Body body) {
+		this.body = body;
+		PVector head = body.getJoint(Body.HEAD);
+		PVector spineBase = body.getJoint(Body.SPINE_BASE);
+		centerX = spineBase.x;
+		centerY = spineBase.y;
+		rad = Math.abs(head.dist(spineBase)); // the Euclidean distance between two points
+	}
+
+	public void draw() {
+		if (isSquare)
+			drawPolygon(4);
+		else
+			drawCircle();
+	}
+
+	public void drawCircle() {
+	}
+
+	public void drawPolygon(int npoints) {
+		float angle = PConstants.TWO_PI / npoints;
+		app.beginShape();
+		for (float a = 0; a < PConstants.TWO_PI; a += angle) {
+			float sx = centerX + PApplet.cos(a) * rad;
+			float sy = centerY + PApplet.sin(a) * rad;
+			app.vertex(sx, sy);
+		}
+		app.endShape(PConstants.CLOSE);
+	}
+
 }
